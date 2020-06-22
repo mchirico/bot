b0142fb1ee372551be173a50d36ce42eb41a74b2
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-06-22 18:17:57 -0400

    update

--- src/mycommands.ts
+++ src/mycommands.ts
@@ -1,25 +1,25 @@
-import { Context } from 'probot'
+import { Context } from "probot";
 
 class MC {
-    context: Context
-    login: string = ''
-    constructor (context: Context) {
-      this.context = context
-    }
+  context: Context;
+  login = "";
+  constructor(context: Context) {
+    this.context = context;
+  }
 
-    get matcher () {
-      return /^\/([\w]+)\b *(.*)?$/m
-    }
+  get matcher(): RegExp {
+    return /^\/([\w]+)\b *(.*)?$/m;
+  }
 
-    scmd (name: string, login: string): string[] {
-      const { comment, issue, pull_request: pr } = this.context.payload
-      const command = (comment || issue || pr).body.match(this.matcher)
-      this.login = this.context?.payload?.comment?.user?.login
-      if (command && name === command[1] && this.login === login) {
-        return command
-      }
-      return []
+  scmd(name: string, login: string): string[] {
+    const { comment, issue, pull_request: pr } = this.context.payload;
+    const command = (comment || issue || pr).body.match(this.matcher);
+    this.login = this.context?.payload?.comment?.user?.login;
+    if (command && name === command[1] && this.login === login) {
+      return command;
     }
+    return [];
+  }
 }
 
-export { MC, Context }
+export { MC, Context };
--- test/comment.test.ts
+++ test/comment.test.ts
@@ -1,58 +1,56 @@
-// You can import your modules
-// import index from '../src/index'
+import nock from "nock";
+import myProbotApp from "../src";
+import { Probot } from "probot";
+import payload from "./fixtures/issues.comment.json";
+import * as fs from "fs";
+import * as path from "path";
+import ErrnoException = NodeJS.ErrnoException;
 
-import nock from 'nock'
-// Requiring our app implementation
-import myProbotApp from '../src'
-import { Probot } from 'probot'
-// Requiring our fixtures
-import payload from './fixtures/issues.comment.json'
+const expectedLabel = ["PigPork"];
 
-const expectedLabel = ['PigPork']
-
-const fs = require('fs')
-const path = require('path')
-
-describe('My Probot app', () => {
-  let probot: any
-  let mockCert: string
+describe("My Probot app", () => {
+  let probot: any;
+  let mockCert: string;
 
   beforeAll((done: Function) => {
-    fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err: Error, cert: string) => {
-      if (err) return done(err)
-      mockCert = cert
-      done()
-    })
-  })
+    fs.readFile(
+      path.join(__dirname, "fixtures/mock-cert.pem"),
+      (err: ErrnoException | null | Error, cert: Buffer) => {
+        if (err) return done(err);
+        mockCert = cert.toString();
+        done();
+      }
+    );
+  });
 
   beforeEach(() => {
-    nock.disableNetConnect()
-    probot = new Probot({ id: 123, cert: mockCert })
+    nock.disableNetConnect();
+    probot = new Probot({ id: 123, cert: mockCert });
     // Load our app into probot
-    probot.load(myProbotApp)
-  })
+    probot.load(myProbotApp);
+  });
 
-  test('creates a label', async (done) => {
-    nock('https://api.github.com')
-      .post('/app/installations/9865425/access_tokens')
-      .reply(200, { token: 'test' })
+  test("creates a label", async (done) => {
+    nock("https://api.github.com")
+      .post("/app/installations/9865425/access_tokens")
+      .reply(200, { token: "test" });
 
     // POST /repos/:owner/:repo/issues/:issue_number/labels
 
     // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/mchirico/bot/issues/43/labels', (body: any) => {
-        done(expect(body).toMatchObject(expectedLabel))
-        return true
+    nock("https://api.github.com")
+      .post("/repos/mchirico/bot/issues/43/labels", (body: any) => {
+        done(expect(body).toMatchObject(expectedLabel));
+        return true;
       })
-      .reply(200)
+      .reply(200);
 
     // Receive a webhook event
-    await probot.receive({ name: 'issue_comment', payload })
-  })
+    await probot.receive({ name: "issue_comment", payload });
+  });
 
   afterEach(() => {
-    nock.cleanAll()
-    nock.enableNetConnect()
-  })
-})
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+});
